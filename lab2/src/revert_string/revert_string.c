ced887aedc9ce77c74a4a7619ed9c007a27bd894
STALKER7777777 <148287810+STALKER7777777@users.noreply.github.com>
2025-11-22 09:29:44 +0300

    Update revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    if (str == NULL) return;
+    
+    int length = strlen(str);
+    int left = 0;
+    int right = length - 1;
+    
+    while (left < right)
+    {
+        // Обмен символов
+        char temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+        
+        left++;
+        right--;
+    }
 }
-
